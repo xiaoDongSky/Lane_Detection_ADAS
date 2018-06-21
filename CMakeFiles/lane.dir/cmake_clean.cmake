1c18947fe1717f3cf863c9e142c2206bdb314c63
anurag-b <anuragbansal4@gmail.com>
2018-06-21 13:43:22 -0400

    First Commit

---
+++ CMakeFiles/lane.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lane.dir/lane_detect.cpp.o"
+  "liblane.pdb"
+  "liblane.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lane.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/lane_detect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lane_detect.dir/main.cpp.o"
+  "lane_detect.pdb"
+  "lane_detect"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lane_detect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
